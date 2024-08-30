54ae29a91aa8f33c14b31356098d2a59165844c9
ferhadeyvazov <eyvazov.ferhad1997@gmail.com>
2024-08-30 18:52:03 +0400

    Search Function was added to main.js and Form was added to html

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
     interval: 2000,
     touch: false
 })
+
+const searchEl = document.getElementById('search_icon');
+const searchForm = document.querySelector('.searchForm');
+
+searchEl.addEventListener('click', ()=>{
+    searchForm.classList.toggle('searchFormActive');
+});
